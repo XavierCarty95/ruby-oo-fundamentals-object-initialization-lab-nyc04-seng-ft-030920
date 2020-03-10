887bf1e396fdda593f008168a0b4310e51216f82
Xavier Carty <xaviercarty@gmail.com>
2020-03-10 21:32:38 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog 
+  
+  def initialize(name=Mutt ,breed)
+    @name = name
+    @breed = breed
+  end 
+  
+  def name=(name)
+    @name = name
+  
+  def name(name)
+     name
+  end 
+  
+  def breed=(breed)
+    @breed = breed
+  end 
+  
+  def breed(breed)
+    breed
+  end
+end 
+
+  
+  
+end
