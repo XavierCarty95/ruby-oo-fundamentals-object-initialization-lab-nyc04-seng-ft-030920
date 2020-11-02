761c4460a09e577f40d61d538c65b168d3d86aaa
XavierCarty95 <xaviercarty@gmail.com>
2020-11-02 12:26:07 -0500

    commit tree 3ef08f515d1d09a241cd5982c551050ba0bc6222

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,16 @@
 class Person 
-  
   def initialize(name)
     @name = name
-  end 
-  
-end
+    
+    end
+    
+    def name=(name)
+       @name = name
+    end
+    
+    
+    def name 
+      @name
+    end
+
+end 
